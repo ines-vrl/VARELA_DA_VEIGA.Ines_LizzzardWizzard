199ce1d6469fbebf56e23be39322cbbdef729981
LucLenne <luc.lenne@outlook.fr>
2024-10-24 12:30:41 +0200

    [ADD]Class Crossbow + bp crossbow + bp bow

---
+++ Source/RogueBoyard/Public/Traps/Crossbow.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RogueTrap.h"
+#include "Crossbow.generated.h"
+
+UCLASS()
+class ROGUEBOYARD_API ACrossbow : public ARogueTrap
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACrossbow();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
