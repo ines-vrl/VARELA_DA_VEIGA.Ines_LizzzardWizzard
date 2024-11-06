d3bbf7085e72cfd8f7e0d8a80d0c5d33ed8a50d7
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-11-06 12:38:49 +0100

    |ADD] Tools PaintWall Base without instantiate Walls

---
+++ Source/RogueBoyard/EditorLib.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EditorLib.h"
+#include "LevelEditor.h"
+
+void AEditorLib::StartListening()
+{
+	FLevelEditorModule* levelEditorModule = FModuleManager::GetModulePtr<FLevelEditorModule>("LevelEditor");
+	if(levelEditorModule != nullptr)
+	levelEditorModule->OnActorSelectionChanged().AddUObject(this, &AEditorLib::HandleActorSelectionChanged);
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to get LevelEditor module."));
+	}
+}
+
+void AEditorLib::StopListening()
+{
+	if(FLevelEditorModule* LevelEditorModule = FModuleManager::GetModulePtr<FLevelEditorModule>("LevelEditor"))
+		LevelEditorModule->OnActorSelectionChanged().RemoveAll(this);
+}
+
+void AEditorLib::HandleActorSelectionChanged(const TArray<UObject*>& NewSelection, bool bForceRefresh)
+{
+	for (UObject* SelectedObject : NewSelection)
+	{
+		if (AActor* SelectedActor = Cast<AActor>(SelectedObject))
+		{
+			UE_LOG(LogTemp, Log, TEXT("Selected Actor: %s"), *SelectedActor->GetName());
+			OnActorSelectionChanged.Broadcast(SelectedActor);
+		}
+	}
+}
---
+++ Source/RogueBoyard/EditorLib.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "EditorLib.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnActorSelectionChanged, AActor*, SelectedActor);
+/**
+ * 
+ */
+UCLASS()
+class ROGUEBOYARD_API AEditorLib : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="Events")
+	FOnActorSelectionChanged OnActorSelectionChanged;
+
+	UFUNCTION(BlueprintCallable, Category="Editor")
+	void StartListening();
+
+	UFUNCTION(BlueprintCallable, Category="Editor")
+	void StopListening();
+
+private:
+	void HandleActorSelectionChanged(const TArray<UObject*>& NewSelection, bool bForceRefresh);
+};
