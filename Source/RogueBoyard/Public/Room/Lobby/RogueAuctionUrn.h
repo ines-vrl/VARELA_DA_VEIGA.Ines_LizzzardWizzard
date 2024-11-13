1b2b309ef1130ee50ae0dac1f6dcc8a7047cd561
Simon-Hubert <s.hubert.mailpro@gmail.com>
2024-11-13 16:31:11 +0100

    Auction Lobby

---
+++ Source/RogueBoyard/Public/Room/Lobby/RogueAuctionUrn.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueAuctionUrn.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class ROGUEBOYARD_API ARogueAuctionUrn : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARogueAuctionUrn();
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* Collider;
+
+	int GetBids() const;
+
+	void AddBid();
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	int Bids;
+};
