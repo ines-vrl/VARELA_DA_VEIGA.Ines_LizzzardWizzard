1cf76553659cfa483ee543936d61569ef6437e07
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-11-19 15:22:17 +0100

    [ADD] Ballon

---
+++ Source/RogueBoyard/Private/Items/Ballon.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Ballon.h"
+
+
+// Sets default values
+ABallon::ABallon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABallon::Floating()
+{
+	FVector Floating = GetActorLocation();
+	Floating.Z = (FloatingRange * FMath::Sin(FloatingFrequency * GetWorld()->GetTimeSeconds())) + Floating.Z;
+	SetActorLocation(Floating);
+}
+
+// Called when the game starts or when spawned
+void ABallon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/RogueBoyard/Public/Items/Ballon.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Ballon.generated.h"
+
+UCLASS()
+class ROGUEBOYARD_API ABallon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABallon();
+
+	UPROPERTY(EditAnywhere)
+	float FloatingFrequency;
+	UPROPERTY(EditAnywhere)
+	float FloatingRange;
+
+	UFUNCTION(BlueprintCallable)
+	void Floating();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
