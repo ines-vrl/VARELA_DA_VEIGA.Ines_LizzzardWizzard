22d9e140685e3050ce523cbfc5560e5154ffc46d
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-11-20 16:59:39 +0100

    [ADD] Ballon

--- Source/RogueBoyard/Private/Items/Ballon.cpp
+++ Source/RogueBoyard/Private/Items/Ballon.cpp
@@ -11,17 +11,59 @@ ABallon::ABallon()
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-void ABallon::Floating()
+void ABallon::MovingOnSin()
 {
 	FVector Floating = GetActorLocation();
-	Floating.Z = (FloatingRange * FMath::Sin(FloatingFrequency * GetWorld()->GetTimeSeconds())) + Floating.Z;
-	SetActorLocation(Floating);
+	float DeltaTime = GetWorld()->DeltaTimeSeconds;
+	
+	SelfTime += DeltaTime;
+	Velocity = Velocity - Velocity * Friction * DeltaTime;
+
+	Floating += Velocity * DeltaTime;
+	
+	float ScaleFrequency = FMath::Lerp(ScaleFrequencyMin, ScaleFrequencyMax, Velocity.Length() * Friction / MaxSpeed);
+	float ScaleRange = FMath::Lerp(ScaleRangeMax,ScaleRangeMin , Velocity.Length() / MaxSpeed);
+	
+	Floating.Z = ScaleRange * FMath::Sin(ScaleFrequency * SelfTime) + PositionStartZ;
+	FVector Scale = StartScale;
+	Scale.X = FMath::Lerp(StartScale.X, StartScale.X + MAXScaleBall, Velocity.Length() * Friction / MaxSpeed );
+	if(Velocity.Length() < MinSpeedToScaleBall)
+	{
+		Scale.X = FMath::Lerp(StartScale.X, StartScale.X + MAXScaleBall, (Velocity.Length() * Friction / MaxSpeed)/2 );
+	}
+
+	SetActorRelativeScale3D(Scale);
+	if(!Velocity.IsNearlyZero())
+	{
+		FRotator Rotator = Velocity.Rotation();
+		Rotator.Pitch = 0.0f;
+		Rotator.Roll = 0.0f;
+		UStaticMeshComponent* StaticMesh = Cast<UStaticMeshComponent>(GetComponentByClass(UStaticMeshComponent::StaticClass()));
+		if(StaticMesh) StaticMesh->SetRelativeRotation(Rotator);
+	}
+	
+	SetActorLocation(Floating, true); 
+}
+
+void ABallon::AddImpulse(FVector DirForce, bool wall)
+{
+	if(wall)
+	{
+		Velocity = DirForce;
+		SelfTime = 0;
+		if(Velocity.Length() > MaxSpeed) Velocity = Velocity.GetSafeNormal() * MaxSpeed;
+		return;
+	}
+	Velocity += DirForce;
+	SelfTime = 0;
+	if(Velocity.Length() > MaxSpeed) Velocity = Velocity.GetSafeNormal() * MaxSpeed;
+	
 }
 
 // Called when the game starts or when spawned
 void ABallon::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	PositionStartZ = GetActorLocation().Z;
 }
 
--- Source/RogueBoyard/Public/Items/Ballon.h
+++ Source/RogueBoyard/Public/Items/Ballon.h
@@ -15,14 +15,56 @@ public:
 	// Sets default values for this actor's properties
 	ABallon();
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MaxSpeed = 2000.0f;
+
 	UPROPERTY(EditAnywhere)
-	float FloatingFrequency;
+	float ScaleRangeMax = 20.0f;
 	UPROPERTY(EditAnywhere)
-	float FloatingRange;
+	float ScaleRangeMin = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+	float ScaleFrequencyMax = 50.0f;
+	UPROPERTY(EditAnywhere)
+	float ScaleFrequencyMin = 4.0f;
+
+	UPROPERTY(EditAnywhere)
+	float MAXScaleBall = 5.0f;
+
+	UPROPERTY(EditAnywhere)
+	float MinSpeedToScaleBall = 1000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float TimeFreezeMax = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float TimeFreezeMin = 0.1f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float EjectionResistance = 1.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	float PositionStartZ;
+	UPROPERTY(BlueprintReadWrite)
+	FVector StartScale;
+	
+	UPROPERTY(BlueprintReadWrite)
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsPushed = false;
+
+	UPROPERTY(EditAnywhere)
+	float Friction = 0.9f;
+	
+	UFUNCTION(BlueprintCallable)
+	void MovingOnSin();
 
 	UFUNCTION(BlueprintCallable)
-	void Floating();
+	void AddImpulse(FVector DirForce, bool wall);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	float SelfTime = 0.0f;
 };
