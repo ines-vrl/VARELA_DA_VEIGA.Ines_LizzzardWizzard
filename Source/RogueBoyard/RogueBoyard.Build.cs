c7a3c1ab2c30153db682a5c7e140f37b6201bfb7
Simon-Hubert <s.hubert.mailpro@gmail.com>
2024-12-05 00:35:25 +0100

    oupsie

--- Source/RogueBoyard/RogueBoyard.Build.cs
+++ Source/RogueBoyard/RogueBoyard.Build.cs
@@ -8,7 +8,7 @@ public class RogueBoyard : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "LocalMultiplayer", "DeveloperSettings", "AkAudio" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "LocalMultiplayer", "DeveloperSettings", "ProceduralMeshComponent",  "AkAudio" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
