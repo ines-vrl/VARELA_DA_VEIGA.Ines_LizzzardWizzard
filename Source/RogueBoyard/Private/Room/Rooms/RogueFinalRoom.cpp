e5116dd59f0a38c73681b0c67cfed3a4ee2edf51
Simon-Hubert <s.hubert.mailpro@gmail.com>
2024-12-04 10:57:17 +0100

    Cpp RogueFinalRoom

---
+++ Source/RogueBoyard/Private/Room/Rooms/RogueFinalRoom.cpp
@@ -0,0 +1,20 @@
+﻿#include "Room/Rooms/RogueFinalRoom.h"
+
+
+ARogueFinalRoom::ARogueFinalRoom() {
+	PrimaryActorTick.bCanEverTick = true;
+	bBeginThePlay = false;
+}
+
+void ARogueFinalRoom::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void ARogueFinalRoom::EndRoom() {
+	Super::EndRoom();
+}
+
+void ARogueFinalRoom::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
